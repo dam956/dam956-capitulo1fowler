9d7d51edd8280d39b50a3a869a36b561fa4474ae
Dani <daniam387@gmail.com>
2016-04-11 19:11:42 +0200

    Añadidos métodos abstractos

--- VideoStore/src/videostore/Price.java
+++ VideoStore/src/videostore/Price.java
@@ -1,39 +1,37 @@
 package videostore;
 
 abstract class Price {
-	   abstract int getPriceCode();
-	   double getCharge(int daysRented) {
-	        double result = 0;
-	        switch (getPriceCode()) {
-	            case Movie.REGULAR:
-	                result += 2;
-	                if (daysRented > 2)
-	                    result += (daysRented - 2) * 1.5;
-	                break;
-	            case Movie.NEW_RELEASE:
-	                result += daysRented * 3;
-	                break;
-	            case Movie.CHILDRENS:
-	                result += 1.5;
-	      if (daysRented > 3)
-	                    result += (daysRented - 3) * 1.5;
-	                break;
-	        }
-	        return result;
-	    }
+	 	abstract double getCharge(int daysRented);
+	 	abstract int getPriceCode();
 	 }
 	 class ChildrensPrice extends Price {
+		 double getCharge(int daysRented){
+	         double result = 1.5;
+	         if (daysRented > 3)
+	             result += (daysRented - 3) * 1.5;
+	         return result;
+	     }
 	   int getPriceCode() {
 	       return Movie.CHILDRENS;
 	  }
 	 }
 	 class NewReleasePrice extends Price {
-	   int getPriceCode() {
+		 double getCharge(int daysRented){
+	         return daysRented * 3;
+	     }
+		 int getPriceCode() {
 	       return Movie.NEW_RELEASE;
 	  }
 	 }
 	 class RegularPrice extends Price {
-	   int getPriceCode() {
+		 double getCharge(int daysRented){
+	         double result = 2;
+	         if (daysRented > 2)
+	             result += (daysRented - 2) * 1.5;
+	         return result;
+	     }
+		 
+		 int getPriceCode() {
 	       return Movie.REGULAR;
-	  }
+		 }
 	  }
